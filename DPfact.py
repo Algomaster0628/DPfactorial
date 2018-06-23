826a83ee6636233c17d115d685632ffcd33d9a9c
Vansh Gupta <37302312+Algomaster1120@users.noreply.github.com>
2018-06-23 21:59:08 +0530

    Create DPfact.py

---
+++ DPfact.py
@@ -0,0 +1,13 @@
+memo = {} # Calculates factorial using memoization and dynammic programming.
+def DPfact(N):
+  if N in memo:
+    return memo[N]
+  if N == 0 or N == 1:
+    return 1
+  else:
+    factorial = N*DPfact(N - 1)
+  memo[N] = factorial
+  return factorial
+x = DPfact(90)
+print(x)
+print(memo)
